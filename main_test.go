37a0ab31a0c05b1c1f9b0c828e2a383d69674824
CamilaAM <88860545+CamilaAM@users.noreply.github.com>
2025-09-29 20:16:05 -0300

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestPingHandler(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, "/pong", nil)
+	req := httptest.NewRequest(http.MethodGet, "/ping", nil)
 	w := httptest.NewRecorder()
 
 	ping(w, req)
@@ -20,7 +20,7 @@ func TestPingHandler(t *testing.T) {
 	}
 
 	body := w.Body.String()
-	expected := "pong"
+	expected := "ping"
 
 	if body != expected {
 		t.Errorf("expected body %q; got %q", expected, body)
