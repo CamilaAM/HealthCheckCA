d9ed7dfc3a92eb10af9a8015f2012016c0eddc38
CamilaAM <88860545+CamilaAM@users.noreply.github.com>
2025-09-29 20:13:14 -0300

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -20,7 +20,7 @@ func TestPingHandler(t *testing.T) {
 	}
 
 	body := w.Body.String()
-	expected := "pong"
+	expected := "ping"
 
 	if body != expected {
 		t.Errorf("expected body %q; got %q", expected, body)
